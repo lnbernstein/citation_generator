0c2c52f46227ea8d1b5f27765fcc36b27e268cd1
Jack Mogen <jmogen@uwaterloo.ca>
2021-04-11 15:23:09 -0400

    Update test.py
    Updating test file with webdriver retrieval

--- test.py
+++ test.py
@@ -2,5 +2,7 @@ import webdriver from selenium
 import Keys from selenium.webdriver.common.keys
 import pandas as pd
 
+driver = webdriver.Chrome('/Users/MyUsername/Downloads/chromedriver')
+
 print('hi')
 print('jtesting')
