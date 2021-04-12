7a43056933629c48bcde840e39ab41e55d683d08
Jack Mogen <jmogen@uwaterloo.ca>
2021-04-12 09:50:36 -0400

    Update test.py
    Function declarations and input prompt added

--- test.py
+++ test.py
@@ -8,3 +8,36 @@ driver.get('https://www.nytimes.com/2021/04/11/world/middleeast/iran-nuclear-nat
 
 print('hi')
 print('jtesting')
+
+def mla(link):
+    driver.get('{}', link)
+    pass 
+
+def apa(link):
+    driver.get('{}', link)
+    pass
+
+citation_style = ''
+hyperlink = ''
+while True:
+    print(
+        'Choose Citation Style:\n'
+        '1. MLA\n'
+        '2. APA\n'
+        '3. Exit\n'
+        )
+    input = citation_style
+
+    print(
+        'Please enter website link:\n'
+        )
+    input = hyperlink
+
+    if citation_style == '1':
+        mla(hyperlink)
+    elif citation_style == '2':
+        apa(hyperlink)
+    elif citation_style == '3':
+        exit()
+    else:
+        print('Invalid choice, please try again')
