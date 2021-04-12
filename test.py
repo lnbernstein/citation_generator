022ceaa489a46c1e58bbacebba99d31b79bf2456
Luke Bernstein <lnbernstein13@gmail.com>
2021-04-11 23:50:02 -0400

    provided test link

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
-import webdriver from selenium
-import Keys from selenium.webdriver.common.keys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import pandas as pd
 
-driver = webdriver.Chrome('/Users/MyUsername/Downloads/chromedriver')
+driver = webdriver.Chrome('/Users/luke/Downloads/chromedriver 2')
+
+driver.get('https://www.nytimes.com/2021/04/11/world/middleeast/iran-nuclear-natanz.html?action=click&module=Spotlight&pgtype=Homepage')
 
 print('hi')
 print('jtesting')
