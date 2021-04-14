bdbc9417840abacde929fd5958c29fcf286aece7
Jack Mogen <jmogen@uwaterloo.ca>
2021-04-13 23:15:15 -0400

    Update test.py
    comment updates

--- test.py
+++ test.py
@@ -18,11 +18,11 @@ def mla(link):
 
     time.sleep(8)  # lets the javascript load in
 
-    # test is a selenium object and we get the actual text by calling test.text
+    
     test = driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/main/div/article/header/div[5]/div/div[2]/p/span[2]/a')
-    print(test.text)
+    print(test.text)  # test is a selenium object and we get the actual text by calling test.text
     test1 = driver.find_element_by_xpath("//span[input/@class='name']")
-    print(test1.text)
+    print(test1.text)  # This code only works on specific websites, why you do this
 
     driver.quit()
     name1 = ''
